f8404758b21250f65175bc5f91b61814c06397cd
Iris-Fla <irith.fla@gmail.com>
2025-01-21 17:18:18 +0900

    approuterに対応した

---
+++ src/app/api/exam/[id]/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import Prisma from "@/lib/prisma";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    const examId = Number(id);
+    if (isNaN(examId)) {
+      return NextResponse.json({ error: "Invalid exam ID" }, { status: 400 });
+    }
+
+    const examData = await Prisma.examdata.findUnique({
+      where: { id: examId },
+    });
+
+    if (!examData) {
+      return NextResponse.json({ error: "Exam not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(examData, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
