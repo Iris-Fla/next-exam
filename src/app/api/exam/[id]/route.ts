c9a89e6d7bfeb745d9d20e3f7365031f7c3ee760
Meru🔮 <irith.fla@gmail.com>
2025-02-05 02:35:07 +0900

    🐛 Buildを通るようにした

--- src/app/api/exam/[id]/route.ts
+++ src/app/api/exam/[id]/route.ts
@@ -3,7 +3,7 @@ import Prisma from "@/lib/prisma";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   try {
